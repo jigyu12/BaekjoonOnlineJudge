772f44e122b201ff69645fb868edbf3ecd010839
pilgyu <pilgyu12@naver.com>
2018-12-12 21:42:12 +0900

    2018-12-12 problem solving 6064

--- src/카잉달력_6064/Main.java
+++ src/카잉달력_6064/Main.java
@@ -1,5 +1,6 @@
 package Ä«À×´Ş·Â_6064;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
@@ -13,97 +14,89 @@ public class Main {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 			int minus = x - y;
-			
+
 			int a = 1;
 			int b = 1;
 			int count = 0;
 			boolean find = false;
+			BigInteger bst = new BigInteger(String.valueOf(start));
+			BigInteger bed = new BigInteger(String.valueOf(end));
+			BigInteger gcd = bst.gcd(bed);
+			bst = bst.divide(gcd);
+			bed = bed.divide(gcd);
+			bst = bst.multiply(bed).multiply(gcd);
 			if (start > end) {
-	//			System.out.println("enter!@#");
+				System.out.println("enter!@#");
 				while (a != start + 1 || b != end + 1) {
-					if(count > start * end) {
+					if (Integer.parseInt(bst.toString()) < count) {
 						break;
 					}
 					if (a - b == minus) {
-						while(a != x || b != y) {
+						while (a != x || b != y) {
 							a++;
 							b++;
 							count++;
-//							System.out.println(a + " " + b + " " + count);
+							System.out.println(a + " " + b + " " + count);
 							find = true;
 							continue;
 						}
 						break;
 					}
-					if(a > b) {
-						int c = (start - a + 1);
-						b = b + c;
-						if(c < 0) {
-							break;
-						}
-						a = 1;
-						count += c;
-//						System.out.println(a + " " + b + " " + count);
+
+					int c = (end - b + 1);
+					a += c;
+					if(a == start) {
+						a++;
+						b++;
+						count++;
 					}
-					else {
-						int c = (end - b + 1);
-						a += c;
-						if(a > start) {
-							a-= start;
-						}
-						b = 1;
-						count += c;
-//						System.out.println(a + " " + b + " " + count);
+					if (a > start) {
+						a -= start;
 					}
+					
+					count += c;
+					System.out.println(a + " " + b + " " + count);
+
 				}
-			} 
-			else if (start <= end) {
-//				System.out.println("enter");
+			} else if (start <= end) {
+				System.out.println("enter");
 				while (a != start + 1 || b != end + 1) {
-					if(count > start * end) {
+					if (Integer.parseInt(bst.toString()) < count) {
 						break;
 					}
 					if (a - b == minus) {
-						while(a != x || b != y) {
+						while (a != x || b != y) {
 							a++;
 							b++;
 							count++;
-//							System.out.println(a + " " + b + " " + count);
+							System.out.println(a + " " + b + " " + count);
 							find = true;
 							continue;
 						}
 						break;
 					}
-					if(a >= b) {
-						int c = (start - a + 1);
-						b = b + c;
-						if(b > end) {
-							b -= end;
-						}
-						a = 1;
-						count += c;
-//						System.out.println(a + " " + b + " " + count);
+
+					int c = (start - a + 1);
+					b += c;
+					if(b == end) {
+						a++;
+						b++;
+						count++;
 					}
-					else {
-						int c = (end - b + 1);
-						a += c;
-						if(c < 0) {
-							break;
-						}
-						b = 1;
-						count += c;
-//						System.out.println(a + " " + b + " " + count);
+					if (b > end) {
+						b -= end;
 					}
+					a = 1;
+					count += c;
+					 System.out.println(a + " " + b + " " + count);
+
 				}
 			}
-			if(find) {
-				System.out.println(count+1);
-			}
-			else {
+			if (find) {
+				System.out.println(count + 1);
+			} else {
 				System.out.println(-1);
 			}
-			
 		}
 	}
-
 }
