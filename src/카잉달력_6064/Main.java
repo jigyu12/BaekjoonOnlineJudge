dc8673689022225ae3a5c3ac4e5ee448766a599e
pilgyu12@naver.com <pilgyu12@naver.com>
2018-12-12 11:25:11 +0900

    2018-12-12 problem solving

--- src/카잉달력_6064/Main.java
+++ src/카잉달력_6064/Main.java
@@ -13,62 +13,91 @@ public class Main {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 			int minus = x - y;
-
+			
 			int a = 1;
 			int b = 1;
 			int count = 0;
 			boolean find = false;
 			if (start > end) {
-				while (a == start + 1 && b == end + 1) {
-					if (a - b == minus) {
-						a++;
-						b++;
-						count++;
-						find = true;
-						continue;
-					}
-					if(a >= b) {
-						int c = (start- a + 1);
-						b = b + c;
-						a = 1;
-						count += c;
-					}
-					else {
-						int c = (end - b + 1);
-						a += c;
-						b = 1;
-					}
-					
-				}
-			} 
-			else if (start <= end) {
+	//			System.out.println("enter!@#");
 				while (a != start + 1 || b != end + 1) {
+					if(count > start * end) {
+						break;
+					}
 					if (a - b == minus) {
 						while(a != x || b != y) {
 							a++;
 							b++;
 							count++;
-							System.out.println(a + " " + b);
+//							System.out.println(a + " " + b + " " + count);
+							find = true;
+							continue;
+						}
+						break;
+					}
+					if(a > b) {
+						int c = (start - a + 1);
+						b = b + c;
+						if(c < 0) {
+							break;
+						}
+						a = 1;
+						count += c;
+//						System.out.println(a + " " + b + " " + count);
+					}
+					else {
+						int c = (end - b + 1);
+						a += c;
+						if(a > start) {
+							a-= start;
+						}
+						b = 1;
+						count += c;
+//						System.out.println(a + " " + b + " " + count);
+					}
+				}
+			} 
+			else if (start <= end) {
+//				System.out.println("enter");
+				while (a != start + 1 || b != end + 1) {
+					if(count > start * end) {
+						break;
+					}
+					if (a - b == minus) {
+						while(a != x || b != y) {
+							a++;
+							b++;
+							count++;
+//							System.out.println(a + " " + b + " " + count);
 							find = true;
 							continue;
 						}
 						break;
 					}
 					if(a >= b) {
-						int c = (start- a + 1);
+						int c = (start - a + 1);
 						b = b + c;
+						if(b > end) {
+							b -= end;
+						}
 						a = 1;
 						count += c;
+//						System.out.println(a + " " + b + " " + count);
 					}
 					else {
 						int c = (end - b + 1);
 						a += c;
+						if(c < 0) {
+							break;
+						}
 						b = 1;
+						count += c;
+//						System.out.println(a + " " + b + " " + count);
 					}
 				}
 			}
 			if(find) {
-				System.out.println(count);
+				System.out.println(count+1);
 			}
 			else {
 				System.out.println(-1);
