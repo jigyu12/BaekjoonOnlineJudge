bf8b5027259ab5414d8fc626a7a5db6cfa16aa5b
pilgyu <pilgyu12@naver.com>
2019-01-11 16:02:53 +0900

    2019-01-11 problem solving

---
+++ src/구간합구하기4_11659/Main.java
@@ -0,0 +1,38 @@
+package 구간합구하기4_11659;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String[] nm = br.readLine().split(" ");
+			
+			int n = Integer.parseInt(nm[0]);
+			int m = Integer.parseInt(nm[1]);
+			int[] sumAr = new int[n];
+			int sum = 0;
+			String[] s = br.readLine().split(" ");
+			for(int i = 0; i < n; i++) {
+				int num = Integer.parseInt(s[i]);
+				sum += num;
+				sumAr[i] = sum;
+			}
+			for(int i = 0; i < m; i++) {
+				String[] t = br.readLine().split(" ");
+				int a = Integer.parseInt(t[0]);
+				int b = Integer.parseInt(t[1]);
+				if(a <= 1) {
+					System.out.println(sumAr[b-1]);
+				}
+				else {
+					System.out.println(sumAr[b-1] - sumAr[a-2]);
+				}
+			}
+		} catch (NumberFormatException | IOException e) {
+		}
+	}
+}
