64ac14fac1105e77f63ca9e57f0bd379e12eab17
pilgyu <pilgyu12@naver.com>
2019-01-11 17:20:28 +0900

    2019-01-11 problem solving

--- src/구간합구하기4_11659/Main.java
+++ src/구간합구하기4_11659/Main.java
@@ -5,32 +5,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	
+	private static int[] ar;
+	private static int[] tree;
+	
+	public static int init(int start, int end, int node) {
+		if(start == end) {
+			return tree[node] = ar[start];
+		}
+		
+		int mid = (start + end) / 2;
+		
+		return tree[node] = init(start, mid,node * 2) + init(mid + 1, end,node * 2 + 1);
+	}
+	
+	public static int sum(int start, int end, int node, int left, int right) {
+		if(left > end || right < start ) {
+			return 0;
+		}
+		
+		if(left <= start && right >= end) {
+			return tree[node];
+		}
+		
+		int mid = (start + end) / 2;
+		
+		return sum(start,mid,node * 2 ,left,right) + sum(mid+1,end,node * 2 + 1,left,right);
+	}
 
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			String[] nm = br.readLine().split(" ");
-			
 			int n = Integer.parseInt(nm[0]);
 			int m = Integer.parseInt(nm[1]); 
-			int[] sumAr = new int[n];
-			int sum = 0;
+			ar = new int[n];
+			tree = new int[4 * n];
 			String[] s = br.readLine().split(" ");
 			for(int i = 0; i < n; i++) {
-				int num = Integer.parseInt(s[i]);
-				sum += num;
-				sumAr[i] = sum;
+				ar[i] = Integer.parseInt(s[i]);
 			}
+			init(0,ar.length - 1, 1);
 			for(int i = 0; i < m; i++) {
 				String[] t = br.readLine().split(" ");
-				int a = Integer.parseInt(t[0]);
-				int b = Integer.parseInt(t[1]);
-				if(a <= 1) {
-					System.out.println(sumAr[b-1]);
-				}
-				else {
-					System.out.println(sumAr[b-1] - sumAr[a-2]);
-				}
+				int a = Integer.parseInt(t[0]) - 1;
+				int b = Integer.parseInt(t[1]) - 1;
+				System.out.println(sum(0,ar.length-1,1,a,b));
 			}
 		} catch (NumberFormatException | IOException e) {
 		}
