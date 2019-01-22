f5f51859be461857fad955e86e2f3e29e6609682
pilgyu <pilgyu12@naver.com>
2019-01-22 21:58:44 +0900

    2019-01-22 problem solving

---
+++ src/LCA2_11438/Main.java
@@ -0,0 +1,34 @@
+package LCA2_11438;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+	
+	private static ArrayList<Integer>[] ar;
+	private static int[] depth;
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int n = Integer.parseInt(br.readLine());
+			ar = new ArrayList[n+1];
+			depth = new int[n+1];
+			for(int i = 0; i < n; i++) {
+				ar[i] = new ArrayList<Integer>();
+			}
+			for(int i = 0; i < n; i++) {
+				String[] s = br.readLine().split(" ");
+				int a = Integer.parseInt(s[0]);
+				int b = Integer.parseInt(s[1]);
+				ar[a].add(b);
+			}
+			
+		} catch (NumberFormatException | IOException e) {
+		}
+		
+	}
+
+}
---
+++ src/게임개발_1516/Main.java
@@ -0,0 +1,62 @@
+package 게임개발_1516;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int test = Integer.parseInt(br.readLine());
+			int[] ans = new int[test];
+			int[] cost = new int[test];
+			int[] indegree = new int[test];
+			Queue<Integer> qu = new LinkedList<Integer>();
+			ArrayList<Integer>[] ar = new ArrayList[test];
+			for(int i = 0; i < test; i++) {
+				ar[i] = new ArrayList<>();
+			}
+			for(int i = 0; i < test; i++) {
+				String[] s = br.readLine().split(" ");
+				ans[i] = 0;
+				cost[i] = Integer.parseInt(s[0]);
+				for(int j = 1; j < s.length-1; j++) {
+					ar[Integer.parseInt(s[j])-1].add(i);
+					indegree[i]++;
+				}
+			}
+	
+			for(int i = 0; i < test; i++) {
+				for(int j = 0; j < test; j++) {
+					if(indegree[j] == 0) {
+						qu.add(j);
+						
+						indegree[j] = -1;
+					}
+				}
+				while(!qu.isEmpty()) {
+					int a = qu.poll();
+					ans[a] += cost[a];
+					for(int b = 0; b < ar[a].size(); b++) {
+						if(ans[a] > ans[ar[a].get(b)]) {
+							ans[ar[a].get(b)] = ans[a];
+						}
+						indegree[ar[a].get(b)]--;
+					}
+				}
+			}
+			for(int i = 0; i < test; i++) {
+				System.out.println(ans[i]);
+			}
+			
+		} catch (NumberFormatException | IOException e) {
+		}
+		
+	}
+
+}
