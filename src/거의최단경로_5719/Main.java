c531db83745c21af128e0e9becdb383fbadd0202
pilgyu <pilgyu12@naver.com>
2019-02-07 21:29:01 +0900

    2019-02-07 problem solving

--- src/거의최단경로_5719/Main.java
+++ src/거의최단경로_5719/Main.java
@@ -1,19 +1,21 @@
 package 거의최단경로_5719;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
-	
-	static class Node{
+
+	static class Node {
 		int end;
 		int cost;
-		
+
 		public Node(int end, int cost) {
 			this.end = end;
 			this.cost = cost;
@@ -25,118 +27,122 @@ public class Main {
 		}
 
 	}
-	
 
-	private static int[][] map;
 	private static int[] dist;
+	private static int[] path;
+	private static int[][] map;
 	private static int n;
 	private static int start;
 	private static int end;
 	private static ArrayList<Node>[] ar;
 	private static PriorityQueue<Node> pq;
-	
-	private static int Dijkstra(int s) {
+
+	private static int dijkstra(int s) {
 		int start = s;
 		Arrays.fill(dist, 987654321);
 		dist[start] = 0;
-		Node node = new Node(start,dist[start]);
-		pq.add(node);
-		int path = -1;
-		while(!pq.isEmpty()) {
-			Node no = pq.poll();
-			
-			for(int i = 0; i < ar[no.end].size(); i++) {
-				if(dist[ar[no.end].get(i).end] > dist[no.end] + ar[no.end].get(i).cost) {
-					dist[ar[no.end].get(i).end] = dist[no.end] + ar[no.end].get(i).cost;
-//					System.out.println(no.end + " " + ar[no.end].get(i).end);
-					if(end == ar[no.end].get(i).end) {
-						path = no.end;
-					}
-					pq.add(new Node(ar[no.end].get(i).end,dist[ar[no.end].get(i).end]));
+		Node no = new Node(start, dist[start]);
+		pq.add(no);
+		while (!pq.isEmpty()) {
+			Node n = pq.poll();
+			for (int i = 0; i < ar[n.end].size(); i++) {
+				Node nn = ar[n.end].get(i);
+				if (dist[nn.end] > dist[n.end] + nn.cost) {
+					dist[nn.end] = dist[n.end] + nn.cost;
+					pq.add(new Node(nn.end, dist[nn.end]));
+					path[nn.end] = n.end;
 				}
 			}
-			
-			
-//			for(int j = 0; j < n; j++) {
-//				System.out.print(dist[j] + " ");
-//			}
-//			System.out.println();
 		}
-		for(int j = 0; j < ar[path].size(); j++) {
-			if(ar[path].get(j).end == end) {
-				ar[path].remove(j);
-			}
-		}
+
 		return dist[end];
 	}
-	
 
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		try {
-			String[] nm = br.readLine().split(" ");
-			n = Integer.parseInt(nm[0]);
-			int m = Integer.parseInt(nm[1]);
-			String[] se = br.readLine().split(" ");
-			start = Integer.parseInt(se[0]);
-			end = Integer.parseInt(se[1]);
-			ar = new ArrayList[n];
-			map = new int[n][n];
-			dist = new int[n];
-			pq = new PriorityQueue<Node>(new Comparator<Node>() {
-				@Override
-				public int compare(Node arg0, Node arg1) {
-					return arg0.cost-arg1.cost;
+			while (true) {
+				String[] nm = br.readLine().split(" ");
+				n = Integer.parseInt(nm[0]);
+				int m = Integer.parseInt(nm[1]);
+				if (n == 0 && m == 0) {
+					break;
 				}
-			});
-			
-			for(int i = 0; i < n; i++) {
-				ar[i] = new ArrayList<Node>();
-			}
+				String[] se = br.readLine().split(" ");
+				start = Integer.parseInt(se[0]);
+				end = Integer.parseInt(se[1]);
+				ar = new ArrayList[n];
+				dist = new int[n];
+				path = new int[n];
+				map = new int[n][n];
+				Arrays.fill(path, -1);
+				pq = new PriorityQueue<Node>(new Comparator<Node>() {
+					@Override
+					public int compare(Node arg0, Node arg1) {
+						return arg0.cost - arg1.cost;
+					}
+				});
 
-			for(int i = 0; i < m; i++) {
-				String[] uvp = br.readLine().split(" ");
-				int u = Integer.parseInt(uvp[0]);
-				int v = Integer.parseInt(uvp[1]);
-				int p = Integer.parseInt(uvp[2]);
-				ar[u].add(new Node(v,p));
-				map[u][v] = p;
-			}
-
-			for(int j = 0; j < n; j++) {
-				System.out.print(ar[j].size() + " ");
-			}
-			System.out.println();
-			int first = Dijkstra(start);
-			System.out.println("first : " + first);
-			for(int j = 0; j < n; j++) {
-				System.out.print(ar[j].size() + " ");
-			}
-			System.out.println();
-//			if(first == 987654321) {
-//				System.out.println("-1");
-//				return ;
-//			}
-			int ans = first;
-			while(ans <= first) {
-				ans = Dijkstra(start);
-				System.out.println(ans);
-				for(int j = 0; j < n; j++) {
-					System.out.print(ar[j].size() + " ");
+				for (int i = 0; i < n; i++) {
+					ar[i] = new ArrayList<Node>();
 				}
-				System.out.println();
-			}
-			if(ans == 987654321) {
-				System.out.println("-1");
-			}
-			else {
-				System.out.println(ans);
-			}
-			
 
+				for (int i = 0; i < m; i++) {
+					String[] uvp = br.readLine().split(" ");
+					int u = Integer.parseInt(uvp[0]);
+					int v = Integer.parseInt(uvp[1]);
+					int p = Integer.parseInt(uvp[2]);
+					map[u][v] = p;
+					ar[u].add(new Node(v, p));
+				}
+
+				int first = dijkstra(start);
+
+				int a = end;
+				int ans = first;
+				while (ans <= first) {
+					a = end;
+					int min = 987654321;
+					int minvertex = -1;
+					for(int i = 0; i < dist.length; i++) {
+						if(min > dist[i] + map[i][end]) {
+							min = dist[i] + map[i][end];
+							minvertex = i;
+							map[i][end] = 987654321;
+						}
+					}
+
+					if(min > first) {
+						break;
+					}
+					while (true) {
+						if (minvertex == -1) {
+							break;
+						}
+						if(a == minvertex) {
+							break;
+						}
+						for (int i = 0; i < ar[minvertex].size(); i++) {
+							if (ar[minvertex].get(i).end == a) {
+								ar[minvertex].remove(i);
+								break;
+							}
+						}
+						a = minvertex;
+						minvertex = path[minvertex];
+					}
+					ans = min;
+				}
+				ans = dijkstra(start);
+				if (ans == 987654321) {
+					bw.write("-1\n");
+				} else {
+					bw.write(ans + "\n");
+				}
+			}
+			bw.flush();
 		} catch (IOException e) {
 		}
-		
 	}
-
 }
