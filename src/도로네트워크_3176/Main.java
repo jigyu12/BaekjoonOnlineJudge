33394a39514d4caa41947e5dd2056ea75999996e
pilgyu <pilgyu12@naver.com>
2019-02-19 21:59:51 +0900

    2019-02-19 problem solving

--- src/도로네트워크_3176/Main.java
+++ src/도로네트워크_3176/Main.java
@@ -1,51 +1,68 @@
-package LCA2_11438;
+package 도로네트워크_3176;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class Main {
 	
-	private static ArrayList<Integer>[] list;
-	private static int[] depth;
-	private static final int pk = 17;
-	private static int[][] parent;
-	
-	private static void dep(int par, int num, int deep) {
-		if(depth[num] != -1) {
-			return ;
+	static class Node{
+		int end;
+		int dist;
+		
+		public Node(int end, int dist) {
+			this.end = end;
+			this.dist = dist;
 		}
-		depth[num] = deep;
-		parent[0][num] = par;
-		for(int a : list[num]) {
-			dep(num,a,deep+1);
+
+		@Override
+		public String toString() {
+			return "Node [end=" + end + ", dist=" + dist + "]";
 		}
 	}
 	
-	public static void main (String [] args) throws Exception {
+	private static ArrayList<Node>[] list;
+	private static final int pk = 17;
+	private static int[] depth;
+	private static int[][] parent;
+	
+	private static void dfs(int par, int sib, int deep) {
+		if(depth[sib] != -1) {
+			return ;
+		}
+		depth[sib] = deep;
+		parent[0][sib] = par;
+		for(Node n : list[sib]) {
+			dfs(sib,n.end,deep+1);
+		}
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int num = Integer.parseInt(br.readLine());
 		list = new ArrayList[num+1];
-		depth = new int[num+1];
 		parent = new int[pk+1][num+1];
- 		for(int i = 0; i < list.length; i++) {
-			list[i] = new ArrayList<Integer>();
-			depth[i] = -1;
+		depth = new int[num+1];
+		for(int i = 0; i < num+1; i++) {
+			list[i] = new ArrayList<Node>();
 			parent[0][i] = -1;
+			depth[i] = -1;
 		}
 		for(int i = 0; i < num-1; i++) {
 			String[] s = br.readLine().split(" ");
 			int a = Integer.parseInt(s[0]);
 			int b = Integer.parseInt(s[1]);
-			list[a].add(b);
-			list[b].add(a);
+			int c = Integer.parseInt(s[2]);
+			list[a].add(new Node(b,c));
+			list[b].add(new Node(a,c));
 		}
 		
-		dep(0,1,0);
-		parent[0][1] = 1;
+		dfs(0,1,0);
+		
 		for(int i = 1; i <= pk; i++) {
 			for(int j = 1; j <= num; j++) {
 				parent[i][j] = parent[i-1][parent[i-1][j]];
@@ -74,21 +91,30 @@ public class Main {
 			}
 			
 			for(int j = pk; j >= 0; j--) {
-				if(a != b && parent[j][b] != parent[j][a]) {
+				if(a != b && parent[j][a] != parent[j][b]) {
 					b = parent[j][b];
 					a = parent[j][a];
 				}
 			}
 			
-			int ans = 0;
+			int lca = 1;
+			
 			if(a == b) {
-				ans = a;
+				lca = a;
 			}
 			else {
-				ans = parent[0][a];
+				lca = parent[0][a];
 			}
-			bw.write(ans+"\n");
+			
 		}
-		bw.flush();
+//		for(int i = 0; i < num+1; i++) {
+//			System.out.print(depth[i] + " ");
+//		}
+//		System.out.println();
+//		for(int i = 0; i < num+1; i++) {
+//			System.out.print(parent[0][i] + " ");
+//		}
+//		System.out.println();
 	}
+
 }
