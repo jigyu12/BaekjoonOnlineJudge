5fc2431cf5ff2d0db3b9816f7950c7a4aed07c27
pilgyu <pilgyu12@naver.com>
2019-02-22 21:50:53 +0900

    2019-02-22 problem solving

--- src/캔디분배_3955/Main.java
+++ src/캔디분배_3955/Main.java
@@ -18,11 +18,37 @@ public class Main {
 				int k = Integer.parseInt(kc[0]);
 				int c = Integer.parseInt(kc[1]);
 				
-				
-				
+				int[] s = new int[50];
+				int[] t = new int[50];
+				int[] r = new int[50];
+				int[] q = new int[50];
+				s[0] = 1;
+				s[1] = 0;
+				t[0] = 0;
+				t[1] = -1;
+				r[0] = c;
+				r[1] = k;
+				int index = 2;
+				while(r[index-1] >= 1) {
+					q[index] = r[index-2] / r[index-1];
+					r[index] = r[index-2] % r[index-1];
+					s[index] = s[index-2] - q[index] * s[index-1];
+					t[index] = t[index-2] - q[index] * t[index-1];
+					index++;
+				}
+				if(k % c == 0) {
+					bw.write("IMPOSSIBLE\n");
+					continue;
+				}
+				if(s[index-1] > 0) {
+					bw.write(s[index-1]+"\n");
+				}
+				else if(s[index-1] < 0) {
+					bw.write(s[index]+"\n");
+				}
 			}
+			bw.flush();
 		} catch (NumberFormatException | IOException e) {
 		}
 	}
-
 }
