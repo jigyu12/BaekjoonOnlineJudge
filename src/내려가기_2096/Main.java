30b088f72b5ad977df9146b2bfb81721bd0617a9
pilgyumin <pilgyu12@naver.com>
2020-09-20 16:58:36 +0900

    2020-09-20
    내려가기

--- src/내려가기_2096/Main.java
+++ src/내려가기_2096/Main.java
@@ -2,91 +2,55 @@ package 내려가기_2096;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			int num = Integer.parseInt(br.readLine());
-			int[][] map = new int[num][3];
-			int[][] max = new int[num][3];
-			int[][] min = new int[num][3];
-			for(int i = 0; i < num; i++) {
-				String[] s = br.readLine().split(" ");
-				for(int j = 0; j < 3; j++) {
-					map[i][j] = Integer.parseInt(s[j]);
-					if(i == 0) {
-						max[i][j] = map[i][j];
-						min[i][j] = map[i][j];
-					}
-				}
-				if(i > 0) {
-					Arrays.fill(min[i], 200000000);
-				}
+		int n = Integer.parseInt(br.readLine());
+		int[][] maxScoreAr = new int[n][3];
+		int[][] minScoreAr = new int[n][3];
+
+		for(int i = 0; i < n; i++){
+			String[] ar = br.readLine().split(" ");
+			int[] input = new int[3];
+			for(int j = 0; j < 3; j++){
+				input[j] = Integer.parseInt(ar[j]);
 			}
-			for(int i = 0; i < num-1; i++) {
-				for(int j = 0; j < 3; j++) {
-					switch(j) {
-					case 0:
-						if(max[i+1][j] < max[i][j] + map[i+1][j]) {
-							max[i+1][j] = max[i][j] + map[i+1][j];
-						}
-						if(max[i+1][j+1] < max[i][j] + map[i+1][j+1]) {
-							max[i+1][j+1] = max[i][j] + map[i+1][j+1];
-						}
-						if(min[i+1][j] > min[i][j] + map[i+1][j]) {
-							min[i+1][j] = min[i][j] + map[i+1][j];
-						}
-						if(min[i+1][j+1] > min[i][j] + map[i+1][j+1]) {
-							min[i+1][j+1] = min[i][j] + map[i+1][j+1];
-						}
-						break;
-					case 1:
-						if(max[i+1][j-1] < max[i][j] + map[i+1][j-1]) {
-							max[i+1][j-1] = max[i][j] + map[i+1][j-1];
-						}
-						if(max[i+1][j] < max[i][j] + map[i+1][j]) {
-							max[i+1][j] = max[i][j] + map[i+1][j];
-						}
-						if(max[i+1][j+1] < max[i][j] + map[i+1][j+1]) {
-							max[i+1][j+1] = max[i][j] + map[i+1][j+1];
-						}
-						if(min[i+1][j-1] > min[i][j] + map[i+1][j-1]) {
-							min[i+1][j-1] = min[i][j] + map[i+1][j-1];
-						}
-						if(min[i+1][j] > min[i][j] + map[i+1][j]) {
-							min[i+1][j] = min[i][j] + map[i+1][j];
-						}
-						if(min[i+1][j+1] > min[i][j] + map[i+1][j+1]) {
-							min[i+1][j+1] = min[i][j] + map[i+1][j+1];
-						}
-						break;
-					case 2:
-						if(max[i+1][j-1] < max[i][j] + map[i+1][j-1]) {
-							max[i+1][j-1] = max[i][j] + map[i+1][j-1];
-						}
-						if(max[i+1][j] < max[i][j] + map[i+1][j]) {
-							max[i+1][j] = max[i][j] + map[i+1][j];
-						}
-						if(min[i+1][j-1] > min[i][j] + map[i+1][j-1]) {
-							min[i+1][j-1] = min[i][j] + map[i+1][j-1];
-						}
-						if(min[i+1][j] > min[i][j] + map[i+1][j]) {
-							min[i+1][j] = min[i][j] + map[i+1][j];
-						}
-						break;
-					}
+			if(i == 0){
+				for(int j = 0; j < 3; j++){
+					maxScoreAr[i][j] = input[j];
+					minScoreAr[i][j] = input[j];
 				}
+				continue;
 			}
-			
-			int ansmax = Math.max(max[num-1][0], Math.max(max[num-1][1], max[num-1][2]));
-			int ansmin = Math.min(min[num-1][0], Math.min(min[num-1][1], min[num-1][2]));
-			System.out.println(ansmax + " " + ansmin);
-		} 
-		catch (NumberFormatException | IOException e) {
+			maxScoreAr[i][0] = input[0] + Math.max(maxScoreAr[i - 1][0], maxScoreAr[i - 1][1]);
+			maxScoreAr[i][1] = input[1] + Math.max(maxScoreAr[i - 1][0],
+					Math.max(maxScoreAr[i - 1][1], maxScoreAr[i - 1][2]));
+			maxScoreAr[i][2] = input[2] + (Math.max(maxScoreAr[i - 1][1], maxScoreAr[i - 1][2]));
+
+			minScoreAr[i][0] = input[0] + (Math.min(minScoreAr[i - 1][0], minScoreAr[i - 1][1]));
+			minScoreAr[i][1] = input[1] + Math.min(minScoreAr[i-1][0],
+					Math.min(minScoreAr[i - 1][1], minScoreAr[i - 1][2]));
+			minScoreAr[i][2] = input[2] + (Math.min(minScoreAr[i - 1][1], minScoreAr[i - 1][2]));
 		}
+
+		int maxScore = 0;
+		int minScore = 1000000;
+
+		for(int j = 0; j < 3; j++){
+			if(maxScore < maxScoreAr[n-1][j]){
+				maxScore = maxScoreAr[n-1][j];
+			}
+
+			if(minScore > minScoreAr[n-1][j]){
+				minScore = minScoreAr[n-1][j];
+			}
+		}
+
+		System.out.println(maxScore + " " + minScore);
 	}
 }
